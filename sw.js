a60eb37472a2a2891ac16ec6a0ce1c6af030b7ae
Romeet Chatterjee <romeet@Romeets-MacBook-Air.local>
2026-03-27 16:24:42 +0530

    PWA: Converted platform to installable Web App with offline support & premium icons

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'designjobs-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
